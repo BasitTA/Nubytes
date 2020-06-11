d664280f77209c8898fbc5c349757a9187ef90ed
Basit <bta.aang@gmail.com>
2020-06-11 09:08:20 +0700

    Initial Commit

---
+++ jajalNC3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jajalNC3
+//
+//  Created by Basit Tri Anggoro on 11/06/20.
+//  Copyright © 2020 Basit Tri Anggoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
